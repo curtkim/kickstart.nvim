62907d938a636906fbc3102639d5afa99b5307dc
curtkim <iamteri@gmail.com>
2024-12-23 11:12:08 +0900

    tree-sitter add cuda

--- init.lua
+++ init.lua
@@ -920,7 +920,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'nickel' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'nickel', 'cuda' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
